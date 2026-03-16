0693cdb5c7994e702681de98c039e49cc248fe0a
Joker <chumalungchamling@gmail.com>
2026-03-16 21:39:35 +0545

    Committed By Chumalung

--- DATABASE/conn.php
+++ DATABASE/conn.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Connected successfully";
+}
+?>
